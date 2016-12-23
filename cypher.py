4cfb1bf714c5ff390e87597164ddea4e006efb80
Jack8Zavitz <Jack8Zavitz@gmail.com>
2016-12-23 15:17:19 +0000

    updated cypher.py and uploaded a testFile.txt

--- cypher.py
+++ cypher.py
@@ -6,12 +6,25 @@
 #There is also the unichr function, returning the Unicode character
 #whose ordinal is the unichr argument:
 
+
+
+def convertString(frase):
+    splitted = list(frase)
+    converted_to_ASCII = []
+    for word in splitted:
+        converted_to_ASCII +=[ord(word)]
+    coded = []
+    for number in converted_to_ASCII:
+        coded +=[number*5682+6]
+    return coded
+
+
+###############################################################
 print ("Please choose one of the following options:")
 print ("\t1)Insert text here")
 print ("\t2)You can put the path to the especific txt file")
 
-print("Option:",)
-option = input()
+option = input("Option: ")
 
 print ("Ok. You choosed option "+option+")")
 
@@ -19,17 +32,18 @@ while option != "1" and option !="2":
     print("Please write 1 or 2 dumbass")
     print ("Option:")
     option=input()
+###############################################################
 if option =="1":
     print("Please write what you want to encript:")
     text=input()
-    splitted = list(text)
-    converted_to_ASCII = []
-    for word in splitted:
-        converted_to_ASCII +=[ord(word)]
-    coded = []
-    for number in converted_to_ASCII:
-        coded +=[number*5682+6]
-    print (coded)
+    print (convertString(text))
+    
 else:
-    print("Please write the path to you txt file:")
-    path=input()
+    path=input("Please write the path to you text file: ")
+    with open(path, "r+") as file:
+        for line in file:
+            print (convertString(line))
+    file.close()
+    
+
+
